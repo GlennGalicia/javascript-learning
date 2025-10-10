dd014b95c58209e6e70928a0fb59f10e90c29e07
glenngalicia <glenn.f.g.a@gmail.com>
2025-10-09 23:27:56 -0600

    Adds user interaction and greeting
    Prompts the user for their name and displays a personalized greeting on the page.
    
    This change enhances user engagement by dynamically displaying a welcome message.

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,6 @@
 console.log("Hola Mundo 9 de Octubre de 2025")
+
+const nombre = prompt("Escribe tu nombre")
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `Hola ${nombre} espero te encuentres bien, bienvenido a JS`
