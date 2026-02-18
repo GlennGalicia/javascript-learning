f958c865ef72bf89c5d030122cd6a850a46a39ef
Glenn Galicia <glenn.f.g.a@gmail.com>
2026-02-18 11:43:30 -0600

    feat: numbers in javascript

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,21 @@
+const numero = 30
+const numero2 = 20
+
+let resultado
+
+// suma
+resultado = numero + numero2
+
+// resta
+resultado = numero - numero2
+
+// multiplicar
+resultado = numero * numero2
+
+// dividir
+resultado = numero / numero2
+
+// modulo
+resultado = numero % numero2
+
+console.log(resultado);
---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,33 @@
+let resultado
+
+// pi
+resultado = Math.PI
+
+// redondeo
+resultado = Math.round(2.8)
+
+// redondeo hacia arriba
+resultado = Math.ceil(2.1)
+
+// redondeo hacia abajo
+resultado = Math.floor(2.9)
+
+// raiz cuadrada
+resultado = Math.sqrt(144)
+
+// absoluto
+resultado = Math.abs(-500)
+
+// potencia
+resultado = Math.pow(2, 4)
+
+// minimo
+resultado = Math.min(10, 15, 1, 0)
+
+// maximo
+resultado = Math.max(10, 15, 1, 0)
+
+// aleatorio
+resultado = Math.random()
+
+console.log(resultado);
